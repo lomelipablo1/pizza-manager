af7139c8433b0198d273b28cc89d839c956aaa69
Pablo Lomeli <lomelipablo1@gmail.com>
2024-10-09 20:50:21 -0700

    fixing remove topping from pizza bug

--- app/controllers/pizzatoppings_controller.rb
+++ app/controllers/pizzatoppings_controller.rb
@@ -2,14 +2,14 @@ class PizzatoppingsController < ApplicationController
     def create
         @pizza = Pizza.find(params[:pizza_id])
         @pizzatopping = @pizza.pizzatoppings.create(pizzatoppings_params)
-        redirect_to pizza_path(@pizza)
+        redirect_to @pizza
     end
 
     def destroy
         @pizzatopping = Pizzatopping.find(params[:id])
         @pizzatopping.destroy
     
-        redirect_to pizza_path(@pizza), status: :see_other
+        redirect_to pizza_path, status: :see_other
     end
 
     private
